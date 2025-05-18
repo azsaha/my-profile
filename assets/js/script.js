23de038368d0bb52b3b5fb1b30b76ed6e9809d37
priontysusmita <priontysusmita0000@gmail.com>
2025-05-18 10:57:23 +0600

    Ommit Errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,7 +144,7 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
+    for (let i = 0; i < pages.length-3; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
